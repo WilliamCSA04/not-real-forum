50fd41d04322778aa432ef84091310f3d3a2c1a4
William <williamcaetano14@gmail.com>
2018-01-23 21:54:22 -0200

    Added default class to button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import "../scss/Button.scss"
 
 class Button extends Component {
 
     render(){
         return(
             <Fragment>
-                <input type="button" className={this.props.classesNames} name={this.props.name} value={this.props.value} onClick={this.props.onClick} />
+                <input type="button" className={`btn ${this.props.classesNames}`} name={this.props.name} value={this.props.value} onClick={this.props.onClick} />
             </Fragment>
         );
     }
