d4a192a6e80204ee1794c43827c8b8c4684f9586
William <williamcaetano14@gmail.com>
2018-01-23 01:05:28 -0200

    Removed default app.js

--- client/src/templates/App.js
+++ client/src/templates/App.js
@@ -4,23 +4,11 @@ import logo from '../images/logo.svg';
 import '../css/App.css';
 import { callApi } from '../actions/api'
 class App extends Component {
-  state = {
-    response: ''
-  };
-  componentDidMount() {
-    callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
   
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
+      <div className="grid-container">
+
       </div>
     );
   }
