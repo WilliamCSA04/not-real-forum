59a6ae4f25bd0cc979aef7757d24da7556a936dd
William <williamcaetano14@gmail.com>
2018-01-23 02:08:18 -0200

    Created header

---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "../scss/Header.scss"
+class Header extends Component {
+  
+  render() {
+    return (
+      <div className="col-12 header">
+      <div className="row">
+        <div className="col-11 col-1-fill">
+            <h1 className="header-title">Not real forum</h1>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+export default Header;
--- client/src/templates/App.js
+++ client/src/templates/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { callApi } from '../actions/api'
+import Header from '../components/Header';
 class App extends Component {
   
   render() {
     return (
       <div className="grid-container">
-
+        <Header />
       </div>
     );
   }
