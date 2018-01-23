5cfa3fda068811fa2c30d840fe40400714d7ef23
William <williamcaetano14@gmail.com>
2018-01-23 01:10:37 -0200

    Replaced all css by scss

--- client/src/templates/App.js
+++ client/src/templates/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
 import logo from '../images/logo.svg';
-import '../css/App.css';
 import { callApi } from '../actions/api'
 class App extends Component {
   
