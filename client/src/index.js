ba33c9bf33eba99e39d4026f653849981eba6c2d
William <williamcaetano14@gmail.com>
2018-01-23 01:11:57 -0200

    Renamed css to scss

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './templates/App';
-import "./css/Default.scss"
+import "./scss/Default.scss"
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
