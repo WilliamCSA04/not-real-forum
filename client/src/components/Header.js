5bd226f0f4d022eae61d3428acf15d9fe11f8f15
William <williamcaetano14@gmail.com>
2018-01-23 23:58:29 -0200

    Added login button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import "../scss/Header.scss"
+import Button from './Button';
 class Header extends Component {
   
   render() {
     return (
       <div className="col-12 header">
       <div className="row">
-        <div className="col-11 col-1-fill vertical-align-middle">
+        <div className="col-5 col-1-offset vertical-align-middle">
             <h1 className="header-title">Not real forum</h1>
+        </div>
+        <div className="col-2 col-4-offset vertical-align-middle">
+          <div>
+            <Button value="login" classesNames="btn-dark"/>
+          </div>
         </div>
       </div>
       </div>
