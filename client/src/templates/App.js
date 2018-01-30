15b87f1d17893603491fc827191f43fd5a3423f3
William <williamcaetano14@gmail.com>
2018-01-29 23:50:10 -0200

    Added ForumDiscussions.js to App

--- client/src/templates/App.js
+++ client/src/templates/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import ForumDiscussions from '../components/ForumDiscussions';
 class App extends Component {
   
   render() {
     return (
       <div className="grid-container">
         <Header />
+        <ForumDiscussions/>
       </div>
     );
   }
