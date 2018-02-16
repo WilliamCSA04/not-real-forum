7e00cbdd6ebe05ec564365ef97ab3805ece0974b
William <williamcaetano14@gmail.com>
2018-02-16 20:39:58 -0200

    Added request at app for forum discussions

--- client/src/templates/App.js
+++ client/src/templates/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import ForumDiscussions from '../components/ForumDiscussions';
+import { recentsDiscussions } from '../actions/api';
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      discussions: []
+    }
+  }
+  
+  componentWillMount() {
+    recentsDiscussions().then(response => {
+      this.setState({ discussions: response });
+    });
+  }
   
   render() {
+    const discussions = this.state.discussions;
     return (
       <div className="grid-container">
         <Header />
-        <ForumDiscussions/>
+        <ForumDiscussions discussions={discussions}/>
       </div>
     );
   }
