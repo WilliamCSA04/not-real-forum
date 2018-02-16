fdd52774c60ee6a4aef9d2b6dc29ee88571aaef8
William <williamcaetano14@gmail.com>
2018-02-16 20:33:21 -0200

    Added example request of discussions to test

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/discussions/recents', (req, res) => {
+  res.send([{ 
+    title: "Title 1",
+    categories: "Category 1",
+    views: "1",
+    commentaries: "Random commentaries",
+    tags: "#example",
+    createdAt: "16/02/2018",
+    creator: "Creator 1",
+  }]);
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
